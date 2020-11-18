f52c2f22a60e26375950ff21b454a2df56b10e2f
Casta <palolahrn0104@gmail.com>
2020-11-18 15:54:53 -0500

    se sube el proyecto

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const actores = require('./routes/actores');
 const modulos = require('./routes/modulos');
-const colegios = require('./routes/colegios');
 
 
-app.set('port', 4001);
+
+app.set('port', process.env.PORT || 4001);
 
 app.use(express.json());
 
 //rutas
 app.use('/api', actores);
 app.use('/api/modulos', modulos);
-app.use('/api/colegios', colegios);
+
+
+//importante para conexion del front con el back
+//Uso de cors
+app.use(cors({origin: '*'}));
+// conectar con el link de su frontend app.use(cors({origin: 'https://ppi-app.vercel.app/'}))
+
+app.get('/',(req,res)=>{
+    res.send('hola este es mi deploy')
+})
 
 
 app.listen(app.get('port'), ()=>{
